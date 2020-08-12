3a67bf298df5d968a1011b524847a6af804836ce
{doulemint} <{871251700@qq.com}>
2020-08-12 11:47:29 -0400

    make more flexible

--- SeepchRecognition.py
+++ SeepchRecognition.py
@@ -2,11 +2,24 @@ from pydub import AudioSegment
 from pydub.utils import make_chunks
 import speech_recognition as sr
 import json
+import os
+
+################################## replace with your files
+audio_file="20200810_meeting.wav" #to be translated file
+path = "20200810_meeting"  # new folds to put all chunks
+##################################
+
+if not os.path.exists(path):
+    os.makedirs(path)
 
 
-text_file = open("transcript.txt","w")
+text_file = open("transcript.txt","w") #the results file
+
 # recognize speech using Google Cloud Speech
-GOOGLE_CLOUD_SPEECH_CREDENTIALS = r"""../cbd00b2d3e72.json"""
+###################################replace with your files
+GOOGLE_CLOUD_SPEECH_CREDENTIALS = r"""cbd00b2d3e72.json"""
+##################################
+
 with open(GOOGLE_CLOUD_SPEECH_CREDENTIALS) as f:
     d = f.read()
 
@@ -17,14 +30,21 @@ end_time_h = 0;
 end_time_m = 0;
 wnd_time_s = 0;
     
-path = ""
 
 
-myaudio = AudioSegment.from_file("Eri_Pres.wav" , "wav") 
+myaudio = AudioSegment.from_file(audio_file , "wav") 
 chunk_length_ms = 30000 # pydub calculates in millisec
 chunks = make_chunks(myaudio, chunk_length_ms) #Make chunks of one sec
 
 #Export all of the individual chunks as wav files
+for i, chunk in enumerate(chunks):
+    chunk_name = os.path.join(path,"chunk{0}.wav".format(i))
+    print ("exporting", chunk_name)
+    chunk.export(path+chunk_name, format="wav")
+    
+
+chunks=[ i for i in os.listdir(path) if i.endswith(".wav")]   
+
 
 for i, chunk in enumerate(chunks):
     print(i)
@@ -40,10 +60,12 @@ for i, chunk in enumerate(chunks):
             end_time_m = end_time_m+1;
             end_time_s = 0;
     r = sr.Recognizer()
+    with sr.AudioFile(os.path.join(path,chunk)) as source:
+        audio = r.record(source)
     try:
         text_file.write("{}:{}:{}----{}:{}:{}\n".format(current_time_h,current_time_m,current_time_s,
                                           end_time_h,end_time_m,end_time_s))
-        text_file.write(r.recognize_google_cloud(chunk, credentials_json=d))
+        text_file.write(r.recognize_google_cloud(audio, credentials_json=d))
         text_file.write('\n')
     except sr.UnknownValueError:
         print("Google Cloud Speech could not understand audio")
